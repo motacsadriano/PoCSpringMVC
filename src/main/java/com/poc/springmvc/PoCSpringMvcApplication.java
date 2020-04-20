65292791e30a617ce9982a2040d6bf237feed49a
Adriano Mota <motacsadriano@gmail.com>
2020-04-20 17:31:35 -0300

    Initial Commit

---
+++ src/main/java/com/poc/springmvc/PoCSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/springmvc/PoCSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
